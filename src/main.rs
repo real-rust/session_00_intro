a363f2de971737f46702aebdd5c03023f513498b
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-06 20:47:51 +0330

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ fn main() {
     let str_2 = "World";
     let str_3 = "from Rust!";
 
+    // string interpolation using println!()
     println!("{} {} {}", str_1, str_2, str_3);
 }
 
+// compile, build and run!
 // rustc main.rc
