7941f8a079f30494123f0dd1e460025d366f619c
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-06 20:50:55 +0330

    Update main.rs
    string interpolation using format!()

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ fn main() {
 
     // string interpolation using println!()
     println!("{} {} {}", str_1, str_2, str_3);
+
+    // string interpolation using format!()
+    let first_name = "Mir Hassan";
+    let last_name = "Moosavi";
+
+    let full_name = format!("{} {} is a Rust programmer!", first_name, last_name);
+    println!("{}", full_name);
+
 }
 
 // compile, build and run!
